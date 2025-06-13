3b3b8247b62e59fc279d27cc85866c1f458850ed
GurrapuPavanKalyan <pavankalyan.gurram@123>
2025-06-13 23:19:38 +0530

    get APIs

--- homework.md
+++ homework.md
@@ -47,3 +47,7 @@
 - Difference b/w JSON and Javascript
 - Add the express.json middleware to your app
 - Make your signUp API dynamic to receive data from the end user
+- User.findOne with duplicate email ids, which object returned?
+- API - Get user by email
+- API - Feed API - GET /feed - get all the users from the database
+
--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const User = require('./models/user');
 app.use(express.json());
 
 app.post('/signup', async (req, res) => {
-    console.log(req.body);
+    //console.log(req.body);
     const user = new User(req.body);
     /* const user = new User({
         firstName: "Virat",
@@ -24,6 +24,36 @@ app.post('/signup', async (req, res) => {
         res.status(400).send("Error saving the user:" + err.message);
     }    
 });
+
+// Get User by EmailId
+app.get('/user', async (req, res) => {
+    const userEmail = req.body.email;
+
+    try {
+        //const users = await User.find({email: userEmail});
+        const users = await User.findOne({email: userEmail});
+        if(users.length === 0) {
+            res.status(404).send("User not found");
+        } else{
+            res.send(users);
+        }
+    } catch(err) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+// Feed API - GET /feed - get all the users from the database
+app.get('/feed', async (req, res) => {
+
+    try {
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
 
 connectDB()
     .then(() => {
